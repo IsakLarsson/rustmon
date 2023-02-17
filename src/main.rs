0ee5c7c015f009b5471c55b6e897ba0b1bc277cf
Isak Larsson <isaklarsson98@hotmail.com>
2023-02-17 19:38:11 +0100

    Options to run away and do nothing

--- src/main.rs
+++ src/main.rs
@@ -30,13 +30,21 @@ enum Type {
 }
 struct Pokemon {
     name: String,
-    power: u32,
+    power: i32,
     poke_type: Type,
     health: i32,
 }
 impl Pokemon {
+    fn new(name: String, power: i32, poke_type: Type, health: i32) -> Self {
+        Self {
+            name,
+            power,
+            poke_type,
+            health,
+        }
+    }
     fn attack(&mut self, defender: &mut Pokemon) {
-        let damage = calculate_damage(&self) as i32;
+        let damage = calculate_damage(&self);
         match self.poke_type {
             Type::Electro => defender.health = defender.health - damage,
             _ => defender.health = defender.health - damage,
@@ -47,13 +55,10 @@ impl Pokemon {
             Colour::Blue.bold().paint(&defender.name),
             damage,
         );
-    }
-    fn new(name: String, power: u32, poke_type: Type, health: i32) -> Self {
-        Self {
-            name,
-            power,
-            poke_type,
-            health,
+        if damage > self.power / 2 {
+            println!("It's super effective")
+        } else {
+            println!("It's mildly effective")
         }
     }
 }
@@ -74,8 +79,12 @@ fn main() {
 
         match Action::from_input(&input) {
             Action::Attack => player.attack(&mut charizard),
-            Action::Run => println!("not implemented"),
-            Action::Nothing => println!("not implemented"),
+            Action::Run => {
+                if run_away() {
+                    return;
+                }
+            }
+            Action::Nothing => println!("You do nothing.."),
         }
         thread::sleep(Duration::from_millis(1000));
         print_health(&player);
@@ -91,9 +100,9 @@ fn greet() {
     println!("For every move you have two options: attack or run");
 }
 
-fn calculate_damage(attacker: &Pokemon) -> u32 {
+fn calculate_damage(attacker: &Pokemon) -> i32 {
     let mut rng = rand::thread_rng();
-    rng.gen_range(1..=attacker.power)
+    rng.gen_range(1..=attacker.power) as i32
 }
 
 fn print_health(pokemon: &Pokemon) {
@@ -108,7 +117,10 @@ fn choose_pokemon() -> Pokemon {
 
     println!("\nChoose your starter, these are your options:\n");
     for pokemon in &pokemon_list {
-        println!("{}", pokemon.name)
+        println!(
+            "{}, Health:{}, Power:{}",
+            pokemon.name, pokemon.health, pokemon.power
+        )
     }
 
     let mut input = String::new();
@@ -116,13 +128,26 @@ fn choose_pokemon() -> Pokemon {
         .read_line(&mut input)
         .expect("Failed to read line!");
 
-    match make_choice(pokemon_list, input) {
-        None => choose_pokemon(), //Is this even allowed???
+    match make_choice(pokemon_list, &input) {
+        None => {
+            println!("Invalid option");
+            choose_pokemon()
+        } //Is this even allowed? Can't possibly be a good practice
         Some(pokemon) => pokemon,
     }
 }
 
-fn make_choice(pokemon_list: Vec<Pokemon>, choice: String) -> Option<Pokemon> {
+fn run_away() -> bool {
+    let mut rng = rand::thread_rng();
+    if rng.gen_range(0..=10) == 10 {
+        println!("You successfully ran away!");
+        true
+    } else {
+        println!("It's not very effective..");
+        false
+    }
+}
+fn make_choice(pokemon_list: Vec<Pokemon>, choice: &str) -> Option<Pokemon> {
     pokemon_list
         .into_iter()
         .find(|pokemon| pokemon.name == choice.trim())
